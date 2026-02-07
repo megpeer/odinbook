8bb4442aa6400eb62708445bdd3a905c048df58c
megpeer <148973963+megpeer@users.noreply.github.com>
2026-02-07 17:28:03 -0500

    cleaned up mailer text

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,9 +1,9 @@
 class WelcomeMailer < ApplicationMailer
   def welcome_email
-    Rails.logger.info "WELCOME MAILER CALLED for #{params[:user].email}"
     @user = params[:user]
     # @user = User.last
     @url = "https://odinbook-yvnb.onrender.com/"
-    mail(to: @user.email, subject: "welcome to my awesome site")
+    # Rails.logger.info "WELCOME MAILER CALLED for #{@user.email}"
+    mail(to: @user.email, subject: "welcome to OdinBook")
   end
 end
--- test/mailers/previews/welcome_mailer_preview.rb
+++ test/mailers/previews/welcome_mailer_preview.rb
@@ -1,6 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/welcome_mailer
 class WelcomeMailerPreview < ActionMailer::Preview
     def welcome_email
-      WelcomeMailer.welcome_email
+      @user = User.last || User.new(name: "test user", email: "test@test.test")
+      WelcomeMailer.with(user: @user).welcome_email
     end
 end
